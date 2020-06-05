a03d3c611983c67bb88a94093ff3d6e31829c357
ChiYeung Law <31592607+ChiYeungLaw@users.noreply.github.com>
2020-06-05 10:45:42 +0800

    new version

--- cut_sentence.py
+++ cut_sentence.py
@@ -1,10 +1,40 @@
-import re
-def cut_sent(para):
-    para = re.sub('([。！？\?])([^”’])', r"\1\n\2", para)  # 单字符断句符
-    para = re.sub('(\.{6})([^”’])', r"\1\n\2", para)  # 英文省略号
-    para = re.sub('(\…{2})([^”’])', r"\1\n\2", para)  # 中文省略号
-    para = re.sub('([。！？\?][”’])([^，。！？\?])', r'\1\n\2', para)
-    # 如果双引号前有终止符，那么双引号才是句子的终点，把分句符\n放到双引号后，注意前面的几句都小心保留了双引号
-    para = para.rstrip()  # 段尾如果有多余的\n就去掉它
-    # 很多规则中会考虑分号;，但是这里我把它忽略不计，破折号、英文双引号等同样忽略，需要的再做些简单调整即可。
-    return para.split("\n")
+def cut_sentence(sentence):
+    endsyms, preidx = [], 0
+    for i in range(len(sentence)):
+        if sentence[i] == '。' or sentence[i] == '！' or sentence[i] == '？':
+            if i+1 < len(sentence) and sentence[i+1] == '”':
+                endsyms.append(sentence[preidx:i+2])
+                preidx = i + 2
+            else:
+                endsyms.append(sentence[preidx:i+1])
+                preidx = i + 1
+
+    # 根据句子长度cut句子
+    cuts = []
+    for line in endsyms:
+        if len(line) >= 5:
+            if len(line) >= 30:
+                list_punct = list(find_all(line, '，'))
+                if list_punct != [] and list_punct[0] != -1:
+                    cut_idx = list_punct[len(list_punct) // 2]
+                    cuts.append(line[:cut_idx])
+                    cuts.append(line[cut_idx+1:])
+                else:
+                    cuts.append(line)
+            else:
+                cuts.append(line)
+
+    # 处理双引号不平衡现象
+    res = []
+    for line in cuts:
+        if "：“" in line and "”" not in line:
+            idx = line.find("“")
+            line = line[idx+1:]
+        elif "”" == line[-1] and "“" not in line:
+            line = line[:-1]
+        if "。" == line[-1] or "！" == line[-1]:
+            line = line[:-1]
+        if len(line) >= 4:
+            res.append(line)
+    
+    return res
